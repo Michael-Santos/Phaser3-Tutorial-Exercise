221f0ecd1a75b362dae30bf63a1fab1caad70349
Michael dos Santos <michael-santos@outlook.com.br>
2023-02-26 21:43:18 -0300

    Add player object and moving mechanism for it

--- Scene1.js
+++ Scene1.js
@@ -6,6 +6,62 @@ class Scene1 extends Phaser.Scene {
     create() {
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship1"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("powerUp", {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 20,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("powerUp", {
+                start: 2,
+                end: 3
+            }),
+            frameRate: 20,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "thurst",
+            frames: this.anims.generateFrameNumbers("player"),
+            frameRate: 20,
+            repeat: -1
+        });
     }
 
     preload() {
@@ -29,6 +85,10 @@ class Scene1 extends Phaser.Scene {
         this.load.spritesheet("powerUp", "assets/spritesheets/power-up.png", {
             frameWidth: 16,
             frameHeight: 16
+        });
+        this.load.spritesheet("player", "assets/spritesheets/player.png", {
+            frameWidth: 16,
+            frameHeight: 24
         });
     }
 
--- Scene2.js
+++ Scene2.js
@@ -11,55 +11,6 @@ class Scene2 extends Phaser.Scene {
         this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
         this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
 
-        this.anims.create({
-            key: "ship1_anim",
-            frames: this.anims.generateFrameNumbers("ship1"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "ship2_anim",
-            frames: this.anims.generateFrameNumbers("ship2"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "ship3_anim",
-            frames: this.anims.generateFrameNumbers("ship3"),
-            frameRate: 20,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: "explode",
-            frames: this.anims.generateFrameNumbers("explosion"),
-            frameRate: 20,
-            repeat: 0,
-            hideOnComplete: true
-        });
-
-        this.anims.create({
-            key: "red",
-            frames: this.anims.generateFrameNumbers("powerUp", {
-                start: 0,
-                end: 1
-            }),
-            frameRate: 20,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: "gray",
-            frames: this.anims.generateFrameNumbers("powerUp", {
-                start: 2,
-                end: 3
-            }),
-            frameRate: 20,
-            repeat: -1,
-        });
-
         this.powerUps = this.physics.add.group();
 
         var maxObjects = 4;
@@ -78,6 +29,13 @@ class Scene2 extends Phaser.Scene {
             powerUp.setCollideWorldBounds(true);
             powerUp.setBounce(1.01);
         }
+
+        this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
+        this.player.play("thurst");
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+        this.player.setCollideWorldBounds(true);
+
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         this.ship1.play("ship1_anim");
         this.ship2.play("ship2_anim");
@@ -109,6 +67,12 @@ class Scene2 extends Phaser.Scene {
         this.moveShip(this.ship3, 2);
 
         this.background.tilePositionY -= 0.5;
+
+        this.movePlayerManager();
+
+        if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+            console.log("Fire!");
+        }
     }
 
     resetShipPos(ship) {
@@ -120,5 +84,19 @@ class Scene2 extends Phaser.Scene {
     destroyShip(pointer, gameObject) {
         gameObject.setTexture("explosion");
         gameObject.play("explode");
+    }
+
+    movePlayerManager() {
+        if(this.cursorKeys.left.isDown) {
+            this.player.setVelocityX(-gameSettings.playerSpeed);
+        } else if (this.cursorKeys.right.isDown) {
+            this.player.setVelocityX(gameSettings.playerSpeed);
+        }
+
+        if(this.cursorKeys.up.isDown) {
+            this.player.setVelocityY(-gameSettings.playerSpeed);
+        } else if (this.cursorKeys.down.isDown) {
+            this.player.setVelocityY(gameSettings.playerSpeed);
+        }
     }
 }
